d91018a4f1682630cd21df8caff9bbbe3eace86d
Gabriel Isobara <jamaltheatlantean@gmail.com>
2023-01-06 05:55:02 +0100

    Updated

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,17 +1,12 @@
 const networkConfig = {
     default: {
         name: "hardhat",
-        interval: "30",
     },
     31337: {
         name: "localhost",
-        interval: "30",
-        callbackGasLimit: "500000", // 500,000 gas
     },
     5: {
         name: "goerli",
-        interval: "30",
-        callbackGasLimit: "500000", // 500,000 gas
     },
   }
   
